b5716e882549e49f3000fb9f1c57faf098a7aef5
codedbykayy <mcloyd.data@gmail.com>
2026-08-29 15:44:58 -0600

    Update faq.js

--- sites/codedbykayys-workshop/data/faq.js
+++ sites/codedbykayys-workshop/data/faq.js
@@ -2,52 +2,44 @@ const faq={
     title:"FAQ",
     questions:[
     {
-        question: "",
-        answer: ""
+        question: "Explain your site to me, I'm confused!",
+        answer: "This website is a custom coded website (by me!) to showcase both my website building talents as well as a combined space to list my other creations. Some of the other things I create include stream pets, stream widgets, OC/Roblox commissions, adopts, and more!"
     },
     {
-        question: "",
-        answer: "" 
+        question: "How long have you been drawing/coding?",
+        answer: "I began coding earlier this year after completing and being bored of my data analytics course. I've been drawing since I could hold a pencil though. Fun fact; I have congenital mirror movement disorder, meaning my hands copy each other, so thats quite fun for drawing and typing lol." 
     },
     {
-        question: "",
-        answer: ""
+        question: "Why would I want a website by you?",
+        answer: "Everything I make is completely custom, including all art assets are drawn entirely by me and so is the writing (I love writing!). Therefore you're not only getting a fully functional website (including mobile!) but an entire array of custom art and themes. Everything is extremely easy to switch out for youself with no coding knowledge required. I've taken months to curate my brand to be as easy for the client as possible upon purchase and will gladly be here to answer any questions."
     },
     {
-        question: "",
-        answer: ""
+        question: "What's a stream pet?",
+        answer: "A stream pet is a cute little animated pet that sits in the corner of your stream and reacts to chat commands and events such as raids. Ive built a fully custom code for my stream pets and am proud of each one I put out as they take quite a bit of drawing work."
     },
     {
-        question: "",
-        answer: ""
+        question: "Do you use AI?",
+        answer: "I build my brand on honesty so thus I'll begin here, being a completely self taught coder trying to compete in a competitive space, I do use AI to answer questions and explain complex concepts to me. But I NEVERR use AI in my art and do not support people using AI art for monetization. I also do not vibe code! I understand every piece of my code, why it's going in, and how to change it at any time."
     },
     {
-        question: "",
-        answer: ""
+        question: "What's the top thing you want to learn next?",
+        answer: "Animation!! I'll be so excited to release more and more polished animated adopts to start before then incorparting animation into my websites!"
     },
     {
-        question: "",
-        answer: ""
+        question: "What can I commission you for?",
+        answer: "My websites; including different packages, custom stream pets, roblox/OC art, and I am down to try anything else that may seem up my alley but if you're requesting something new to me please have patience!"
     },
     {
-        question: "",
-        answer: ""
+        question: "What programs do you use?",
+        answer: "For coding I use VSCode and Github. For drawing I use Procreate (and a little dreams) and Aesprite but I plan to learn alightmotion soon!"
     },
     {
-        question: "",
-        answer: ""
+        question: "What devices do you have?",
+        answer: "I have a very old ipad (from 2019) and apple pencil but plan to upgrade soon to the Ipad Air. My PC is the star of the show though with dual monitors, a 5060, and 32gb ram. teeheeee."
     },
     {
-        question: "",
-        answer: ""
+        question: "Interested in hiring me for any bigger roles?",
+        answer: "I'd love to! My biggest dream is to be a game designer (I've tried to make my own but goodness working by yourself is hard!) I'd be thrilled to join a real team of developers/artists!"
     },
-    {
-        question: "",
-        answer: ""
-    },
-    {
-        question: "",
-        answer: ""
-    }
 ]
 }
