8cb332fba49e651b5514a5f3562a407387465f21
codedbykayy <mcloyd.data@gmail.com>
2026-08-29 17:04:43 -0600

    calenfar works

--- sites/codedbykayys-workshop/data/calendar.js
+++ sites/codedbykayys-workshop/data/calendar.js
@@ -1,3 +1,3 @@
 const calendar = {
-    embedLink:src="", 
+    embedLink:"https://calendar.google.com/calendar/embed?src=788a964ae48e28f16c5d1f875c83d7cd304985cf1320d025ebeac421896b4760%40group.calendar.google.com&ctz=America%2FDenver" 
 }
