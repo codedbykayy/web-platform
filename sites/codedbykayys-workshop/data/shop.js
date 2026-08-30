68c8cdd94a82077400c7874c6e14f85f9096ba7b
codedbykayy <mcloyd.data@gmail.com>
2026-08-30 15:37:06 -0600

    shop done?

--- sites/codedbykayys-workshop/data/shop.js
+++ sites/codedbykayys-workshop/data/shop.js
@@ -6,49 +6,76 @@ const shop = {
     },
     items: [
         {
-            image: "../../assets/toymakerhero.png",
-            alt: "Example shop listing",
-            title: "Example Listing",
-            description: "A sample listing so we can test the shop layout.",
-            price: "$25",
-            buttonText: "Dibs!",
-            buttonLink: "#"
+            image: "../../assets/strawberryturtle.png",
+            alt: "Strawberry Dessert Turtle Stream Pet",
+            title: "Strawberry Dessert Turtle Stream Pet",
+            description: "A cute Dessert Turtle pet who lives in the corner of your stream and reacts to chat!",
+            price: "$5",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/4555073175/strawberry-shortcake-turtle-stream-pet?ls=r&ref=hp_recent_activity_hub-5&sr_prefetch=1&pf_from=home_non_recs&content_source=c10d6968a823a19105e73e3b2c547c87%253ALTdf7fb933da935b8365123beab5278a2a28b70f95&logging_key=c10d6968a823a19105e73e3b2c547c87%3ALTdf7fb933da935b8365123beab5278a2a28b70f95"
         },
         {
-            image: "../../assets/toymakerhero.png",
+            image: "../../assets/lemonturtle.png",
             alt: "Example shop listing",
-            title: "Example Listing",
-            description: "A sample listing so we can test the shop layout.",
-            price: "$35",
-            buttonText: "Buy Now!",
-            buttonLink: "#"
+            title: "Lemon Meringue Dessert Turtle Stream Pet",
+            description: "A cute Dessert Turtle pet who lives in the corner of your stream and reacts to chat!",
+            price: "$5",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/4555089782/lemon-meringue-turtle-stream-pet?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-3&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT2dd55bcb2fda75f7815803917670b5a1eec08c37&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT2dd55bcb2fda75f7815803917670b5a1eec08c37"
         },
         {
-            image: "../../assets/toymakerhero.png",
+            image: "../../assets/mintturtle.png",
             alt: "Example shop listing",
-            title: "Example Listing",
-            description: "A sample listing so we can test the shop layout.",
-            price: "$45",
-            buttonText: "View Listing",
-            buttonLink: "#"
+            title: "MintChip Dessert Turtle Stream Pet",
+            description: "A cute Dessert Turtle pet who lives in the corner of your stream and reacts to chat!",
+            price: "$5",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/4555081279/mintchip-turtle-stream-pet?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-7&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT41920047b54aeac24780a25e89c20e3211b4dae1&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT41920047b54aeac24780a25e89c20e3211b4dae1"
         },
         {
-            image: "../../assets/toymakerhero.png",
+            image: "../../assets/blueberryturtle.png",
             alt: "Example shop listing",
-            title: "Example Listing",
-            description: "A sample listing so we can test the shop layout.",
-            price: "$55",
-            buttonText: "Shop Now",
-            buttonLink: "#"
+            title: "Blueberry Cheesecake Dessert Turtle Stream Pet",
+            description: "A cute Dessert Turtle pet who lives in the corner of your stream and reacts to chat!",
+            price: "$5",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/4553905737/blueberry-cheesecake-turtle-stream-pet?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-2&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT83824926be103571481d2474e4b374d5e74b1fec&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT83824926be103571481d2474e4b374d5e74b1fec"
         },
         {
-            image: "../../assets/toymakerhero.png",
+            image: "../../assets/velvetturtle.png",
             alt: "Example shop listing",
-            title: "Example Listing",
-            description: "A sample listing so we can test the shop layout.",
-            price: "$65",
-            buttonText: "Dibs!",
-            buttonLink: "#"
-        }
+            title: "Red Velvet Dessert Turtle Stream Pet",
+            description: "A cute Dessert Turtle pet who lives in the corner of your stream and reacts to chat!",
+            price: "$5",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/4553902705/red-velvet-turtle-stream-pet?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-4&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT60649715c3c831e6b2c2b9873d095ef684b6f676&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT60649715c3c831e6b2c2b9873d095ef684b6f676"
+        },
+        {
+            image: "../../assets/pastelturtles.png",
+            alt: "Example shop listing",
+            title: "Pastel Dessert Turtle Stream Pet 3 Pack!",
+            description: "A 3 pack of cute Dessert Turtle pets who lives in the corner of your stream and reacts to chat!",
+            price: "$12.50",
+            buttonText: "I want them!",
+            buttonLink: "https://www.etsy.com/listing/4555102048/pastel-platter-turtle-stream-pets-3-pack?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-5&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT9e897c8f423b9823a43de5a0f726d052b09ef6be&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT9e897c8f423b9823a43de5a0f726d052b09ef6be"
+        },
+        {
+            image: "../../assets/allturtles.png",
+            alt: "Example shop listing",
+            title: "Rainbow Platter Dessert Turtle Stream Pet 5 Pack!",
+            description: "A cute 5 pack of Turtle Dessert pets who lives in the corner of your stream and reacts to chat!",
+            price: "$15",
+            buttonText: "I want them all!",
+            buttonLink: "https://www.etsy.com/listing/4555087077/rainbow-rush-dessert-turtle-stream-pet-5?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-8&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALT5380fd09c1b505f56733eacafbd7621f622a87cb&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALT5380fd09c1b505f56733eacafbd7621f622a87cb"
+        },
+        {
+            image: "../../assets/bearsleep.png",
+            alt: "Example shop listing",
+            title: "Sleeping Bear Emote",
+            description: "A cute sleeping bear emote, has animated version!",
+            price: "$2.50",
+            buttonText: "I want one!",
+            buttonLink: "https://www.etsy.com/listing/1307229078/sleeping-bear-twitch-emote?ls=r&sr_prefetch=1&pf_from=shop_home&ref=items-pagination-10&dd=1&content_source=68d6416b1b48d7cb86bc68e47524518d%253ALTe0a5232f186b28d62a51998d71ecf5bcb5b699b0&logging_key=68d6416b1b48d7cb86bc68e47524518d%3ALTe0a5232f186b28d62a51998d71ecf5bcb5b699b0"
+        },
     ]
 };
