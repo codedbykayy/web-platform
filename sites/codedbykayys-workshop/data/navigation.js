2372a7b89618ceb6e357dc486d55fa1b95982f2d
codedbykayy <mcloyd.data@gmail.com>
2026-08-30 17:26:10 -0600

    Update navigation.js

--- sites/codedbykayys-workshop/data/navigation.js
+++ sites/codedbykayys-workshop/data/navigation.js
@@ -12,7 +12,7 @@ const navigation = {
         {label: "FAQ", link:"pages/faq/index.html"},
         {label: "Shop", link:"pages/shop/index.html"},
         {label: "Gallery", link:"pages/gallery/index.html"},
-        {label: "Request Form", link:""}
+        {label: "Request a Website", link:""}
     ],
     button:{
         label:"Shop my Etsy",
