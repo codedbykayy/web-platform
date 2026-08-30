20225118af1179e332b705155e2a26a0af8aed0b
codedbykayy <mcloyd.data@gmail.com>
2026-08-30 13:45:51 -0600

    updated gallery

--- sites/codedbykayys-workshop/data/gallery.js
+++ sites/codedbykayys-workshop/data/gallery.js
@@ -5,37 +5,37 @@ const gallery = {
         description: "A collection of my creations, projects, and videos."
     },
     images: {
-        title: "Featured Images",
+        title: "Featured Art!",
         items: [
             {
                 image: "../../assets/toymakerhero.png",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Toymaker OC"
             },
             {
                 image: "../../assets/moonsnailundertale.gif",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Moonsnail Oc, First Animation!"
             },
             {
                 image: "../../assets/kaori.png",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Kokiri inspired OC, iykyk teehee"
             },
             {
                 image: "../../assets/applecomm.png",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Cute Apple-Themed Roblox Comm"
             },
             {
-                image: "../../assets/bearsleep.png",
+                image: "../../assets/streampets.gif",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "First Stream Pets!! On Etsy!: CodeWithKayy"
             },
             {
                 image: "../../assets/purplecomm.png",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Pretty Roblox Comm"
             }
         ]
     },
@@ -45,17 +45,17 @@ const gallery = {
             {
                 video: "../../assets/toymaker-speedpaint.mov",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Toymaker OC Speedpaint"
             },
             {
                 video: "../../assets/kaorispeedpaint.mov",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Kaori Speedpaint"
             },
             {
                 video: "../../assets/applecommspeedpaint.mov",
                 alt: "Featured image",
-                title: "Featured Piece"
+                title: "Apple Comm Speedpaint"
             },
         ]
     }
